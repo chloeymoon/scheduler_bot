9b10cca479beec2b64a5f9a831d4ef42861686ea
Elise Yang <elisenyang@Elises-MacBook-Pro-2.local>
2017-07-18 14:01:54 -0700

    jfiwojw

--- env.sh
+++
@@ -1,3 +0,0 @@
-export SLACK_BOT_TOKEN='xoxb-215124260887-v9dFY9wAVxyhnounQamsFMhn'
-export API_AI_TOKEN='e919c82598b54f8fa522bf1b2bad61e8'
-export MONGODB_URI=mongodb://elise:elise@ds163232.mlab.com:63232/schedulerbot
